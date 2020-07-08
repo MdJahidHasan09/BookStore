1064913da0f4c9027ed67751c6987447e118fb4f
MD Jahid Hasan <jahid60012@gmail.com>
2020-07-08 20:08:35 +0600

    initial author routes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 const PORT = 3000;
 
@@ -12,6 +14,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extends: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -22,5 +25,7 @@ db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
 
 app.listen(process.env.PORT || PORT);
